0c6a8b3b287d940208ff8ab1a67f8f7148a4f7ab
ephemeralwaves <thinkcolorful@thinkcolorful.org>
2019-01-09 17:58:58 -0500

    Added test script echo_flow_post.js

---
+++ echo_flow_post.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var assert = require ('assert'),
+	EchoPage = require ( '../pageobjects/echo.page' ),
+	UserLoginPage = require( 'wdio-mediawiki/LoginPage' ),
+	CreateAccountPage = require( '../../../../../tests/selenium/pageobjects/createaccount.page' ),
+    Util = require( 'wdio-mediawiki/Util' ),
+	Api = require( 'wdio-mediawiki/Api' );
+
+describe( 'Echo', function () {
+
+	it( 'Admin creates a new user', function () {
+        var password,
+            username;
+        username = Util.getTestString( 'NewUser-' );
+		password = Util.getTestString();
+        
+        //Given there is an admin user on the Create Account page
+        UserLoginPage.login( browser.options.username, browser.options.password );
+		UserLoginPage.open();
+        //When I enter a new user name and password and submit
+		CreateAccountPage.createAccount( username, password )
+        //Then a new user is created
+        assert.strictEqual( CreateAccountPage.heading.getText(), `Welcome, ${username}!` );
+
+    } );
+
+} );

383bc633380abf3e37c179aaa42265ae28b0c9bf
ephemeralwaves <thinkcolorful@thinkcolorful.org>
2019-02-13 23:31:42 -0500

    Fixed mention test, still failing on fourth test

--- features/mention.feature
+++ features/mention.feature
@@ -2,27 +2,26 @@ Feature: User Mention Notifications
  Background: 
     Given an admin user
     
-  Scenario: Admin mentions new user on talk page
-    Given admin creates new user
-    When admin goes to their talk page 
+Scenario: New user mentions admin on admin user page
+    Given admin creates new user and they are logged in
+    When the new user edits the admin user's page
     And mention new user
-    Then the new user mention is visible on talk page
-    
-  Scenario: New User sees mention
-    Given new user is logged in
-    When new user clicks on the notification badge
-    Then new user see they was mentioned
-    
-  Scenario: New user mentions admin on talk page
-    Given new user is logged in
-    When new user go to their talk page 
-    And mention the admin user
-    Then the admin user mention is visible on talk page
-  
+    Then the admin mention is visible on the admin user's page
+
   Scenario: Admin sees mention
     Given admin is logged in
     When admin clicks on the notification badge
     Then admin see they were mentioned
-    
+
+  Scenario: Admin mentions new user on new user page
+    Given admin is logged in
+    When admin goes to the new user user's page
+    And mentions the new user
+    Then the new user mention is visible on new user's page
+
+  Scenario: New user sees mention
+    Given new user is logged in
+    When new user clicks on the notification badge
+    Then new user sees they were mentioned
 
 
--- specs/mention.js
+++ specs/mention.js
@@ -1,59 +1,80 @@
-  /*Scenario: Admin mentions new user on talk page
-    Given admin creates new user and they are logged in
-    When the new user edits the admin user's page
-    And mention new user
-    Then the admin mention is visible on the admin user's page
-
-  Scenario: Admin sees mention
-    Given admin is logged in
-    When admin clicks on the notification badge
-    Then admin see they were mentioned
-
-  Scenario: Admin mentions new user on their user page
-    Given admin is logged in
-    When admin goes to the new user user's page
-    And mentions the new user
-    Then the new user mention is visible on new user's page
-
-  Scenario: New user sees mention
-    Given new user is logged in
-    When new user clicks on the notification badge
-    Then new user sees they were mentioned
-	*/
 'use strict';
 
 var assert = require ('assert'),
 	EchoPage = require ( '../pageobjects/echo.page' ),
 	UserLoginPage = require( 'wdio-mediawiki/LoginPage' ),
 	CreateAccountPage = require( '../../../../../tests/selenium/pageobjects/createaccount.page' ),
-	//Api = require( 'wdio-mediawiki/Api' ),
 	EditPage = require( '../../../../../tests/selenium/pageobjects/edit.page' ),
     Util = require( 'wdio-mediawiki/Util' );
-
 
 
 describe( 'Mention test for Echo', function () {
 	var password,
     username;
 
+    /*
+    Given admin creates new user and they are logged in
+    When the new user edits the admin user's page
+    And mention new user
+    Then the admin mention is visible on the admin user's page
+    */
 
-	it( 'should be able to create account', function () {
+	it( 'mentions admin on admin user page', function () {
+
 		username = Util.getTestString( 'User-' );
 		password = Util.getTestString();
-		// create
 		CreateAccountPage.createAccount( username, password );
-		EditPage.edit(`User:${username}`, `Hello [[User:Admin]] ~~~~` )
-		EchoPage.alerts.click();
-        EchoPage.alertText.waitForVisible();
-        assert( EchoPage.alertText.isExisting() );
+		EditPage.edit(`User:${username}`, `Hola [[User:Admin]] ~~~~` )
+		EchoPage.userMention.waitForVisible();
+		assert( EchoPage.userMention.isExisting() );
+
+	});
+
+	/*
+    Given admin is logged in
+    When admin clicks on the notification badge
+    Then admin see they were mentioned
+	*/
+
+	it( 'checks if admin sees mention', function () {
 
      	UserLoginPage.login( browser.options.username, browser.options.password );
 		UserLoginPage.open();
-		EditPage.edit('User:Admin', `Hello [[User:${username}]] ~~~~` )
 		EchoPage.alerts.click();
         EchoPage.alertText.waitForVisible();
         assert( EchoPage.alertText.isExisting() );
 
+	} );
+	/*
+    Given admin is logged in
+    When admin goes to the new user user's page
+    And mentions the new user
+    Then the new user mention is visible on new user's page
+	*/
+
+	it( 'mentions new user on user page', function () {
+
+		EditPage.edit('User:Admin', `Hola [[User:${username}]] ~~~~` )
+		EchoPage.userMention.waitForVisible();
+		assert( EchoPage.userMention.isExisting() );
+
+	} );
+
+	/*
+	Scenario: New user sees mention
+    Given new user is logged in
+    When new user clicks on the notification badge
+    Then new user sees they were mentioned
+	*/
+
+	it( 'checks if new user sees mention', function () {
+
+     	UserLoginPage.login( username,password );
+		//UserLoginPage.open();
+		EchoPage.open();
+		EchoPage.alerts.click();
+        EchoPage.alertText.waitForVisible();
+        assert( EchoPage.alertText.isExisting() );
 
 	} );
 

db0d735320653047535f850c862b75297eb7a3e3
ephemeralwaves <thinkcolorful@thinkcolorful.org>
2019-02-20 12:43:41 -0500

    Fixed mention.js so tests work and updated echo.page.js

--- pageobjects/echo.page.js
+++ pageobjects/echo.page.js
@@ -1,73 +1,24 @@
 'use strict';
+const Page = require( 'wdio-mediawiki/Page' );
 
-var assert = require ('assert'),
-	EchoPage = require ( '../pageobjects/echo.page' ),
-	UserLoginPage = require( 'wdio-mediawiki/LoginPage' ),
-	CreateAccountPage = require( '../../../../../tests/selenium/pageobjects/createaccount.page' ),
-    Util = require( 'wdio-mediawiki/Util' );
-    
-var password,
-    username;
+class EchoPage extends Page {
 
-describe( 'Smoke test for Echo', function () {
-    
-    /**
-     *Given there is an admin user on the Create Account page
-     *When I enter a new user name and password and submit
-     *Then a new user is created
-	 */
+        get alerts() { return browser.element( '#pt-notifications-alert' ); }
+        get notices() { return browser.element( '#pt-notifications-notice' ); }
+        get welcomeNotice() { return browser.element( '.mw-echo-ui-notificationItemWidget-content-message-header' ); }
+        get newUserConfirm() { return browser.element( '#mw-content-text' ); }
+        get newUserNav() { return browser.element( '#pt-userpage' ); }
+        get notificationHeading() { return browser.element( '#firstHeading' ); }
+        get notificationsCount() {return browser.element('.mw-echo-ui-paginationWidget-label');}
+	    get alertText() { return browser.element( '.mw-echo-ui-notificationItemWidget-content-message-header' ); }
+		get userMention() { return browser.element('.mw-selflink' );}
+        open() {
+                super.openTitle( 'Special:Notifications', { uselang: 'en' } );
+        }
 
-	it( 'admin creates a new user', function () {
-        username = Util.getTestString( 'NewUser-' );
-		password = Util.getTestString();
-        
-        UserLoginPage.login( browser.options.username, browser.options.password );
-		UserLoginPage.open();
-		CreateAccountPage.createAccount( username, password );
-        assert.strictEqual(EchoPage.newUserConfirm.getText(), `The user account for ${username} (talk) has been created.\nReturn to Special:CreateAccount.`);
-    } );
-    
-    /**
-     *Given admin created a new user
-     *When the new user enters credentials on the login page
-     *Then the new user is logged in
-	 */
-    
-    it( 'new user logs in', function () {   
-        UserLoginPage.login(username, password);
-        UserLoginPage.open();
-        assert.strictEqual(EchoPage.newUserNav.getText(), `${username}`);
-    } );
-    
-    /**
-     *Given I am a new logged in user
-     *Then alerts and notices are visible.
-     */
-    it( 'alerts and notices are visible after logging in', function () {
-        assert( EchoPage.alerts.isExisting() );
-        assert( EchoPage.notices.isExisting() );
-        } );
-    
-    /**
-     *Given I am a new logged in user
-     *When I click the notices icon
-     *Then I see a welcome message
-     */
-    it( 'checks for welcome message after signin', function () {
-        EchoPage.notices.click();
-        EchoPage.welcomeNotice.waitForVisible();
-        assert( EchoPage.welcomeNotice.isExisting() );
-        } );
-    /**
-     *Given I am a new logged in user
-     *When I go to the Notifications Page
-     *Then I see the Notification title
-     */
-    it( 'checks for Notifications Page', function () {
-        
-        EchoPage.open();
-        assert.strictEqual(EchoPage.notificationHeading.getText(), `Notifications`);
-        } );
-    
-    
-} );
+}
+module.exports = new EchoPage();
+
+
+
+
--- specs/mention.js
+++ specs/mention.js
@@ -1,81 +1,78 @@
 'use strict';
 
-var assert = require ('assert'),
-	EchoPage = require ( '../pageobjects/echo.page' ),
+var assert = require( 'assert' ),
+	EchoPage = require( '../pageobjects/echo.page' ),
 	UserLoginPage = require( 'wdio-mediawiki/LoginPage' ),
 	CreateAccountPage = require( '../../../../../tests/selenium/pageobjects/createaccount.page' ),
 	EditPage = require( '../../../../../tests/selenium/pageobjects/edit.page' ),
-    Util = require( 'wdio-mediawiki/Util' );
-
+	Util = require( 'wdio-mediawiki/Util' );
 
 describe( 'Mention test for Echo', function () {
 	var password,
-    username;
+		username;
 
-    /*
-    Given admin creates new user and they are logged in
-    When the new user edits the admin user's page
-    And mention new user
-    Then the admin mention is visible on the admin user's page
-    */
+	/**
+     *Given admin creates new user and they are logged in
+     *When the new user edits the admin user's page
+     *And mention new user
+     *Then the admin mention is visible on the admin user's page
+     */
 
 	it( 'mentions admin on admin user page', function () {
 
 		username = Util.getTestString( 'User-' );
 		password = Util.getTestString();
 		CreateAccountPage.createAccount( username, password );
-		EditPage.edit(`User:${username}`, `Hola [[User:Admin]] ~~~~` )
+		EditPage.edit( `User:${username}`, 'Hello [[User:Admin]] ~~~~' );
 		EchoPage.userMention.waitForVisible();
 		assert( EchoPage.userMention.isExisting() );
 
-	});
+	} );
 
-	/*
-    Given admin is logged in
-    When admin clicks on the notification badge
-    Then admin see they were mentioned
-	*/
+	/**
+     *Given admin is logged in
+     *When admin clicks on the notification badge
+     *Then admin see they were mentioned
+     */
 
 	it( 'checks if admin sees mention', function () {
 
-     	UserLoginPage.login( browser.options.username, browser.options.password );
+		UserLoginPage.login( browser.options.username, browser.options.password );
 		UserLoginPage.open();
 		EchoPage.alerts.click();
-        EchoPage.alertText.waitForVisible();
-        assert( EchoPage.alertText.isExisting() );
+		EchoPage.alertText.waitForVisible();
+		assert( EchoPage.alertText.isExisting() );
 
 	} );
-	/*
-    Given admin is logged in
-    When admin goes to the new user user's page
-    And mentions the new user
-    Then the new user mention is visible on new user's page
-	*/
+	/**
+     *Given admin is logged in
+     *When admin goes to the new user user's page
+     *And mentions the new user
+     *Then the new user mention is visible on new user's page
+     */
 
 	it( 'mentions new user on user page', function () {
 
-		EditPage.edit('User:Admin', `Hola [[User:${username}]] ~~~~` )
+		EditPage.edit( 'User:Admin', `Hello [[User:${username}]] ~~~~` );
 		EchoPage.userMention.waitForVisible();
 		assert( EchoPage.userMention.isExisting() );
 
 	} );
 
-	/*
-	Scenario: New user sees mention
-    Given new user is logged in
-    When new user clicks on the notification badge
-    Then new user sees they were mentioned
-	*/
+	/**
+     *Given new user is logged in
+     *When new user clicks on the notification badge
+     *Then new user sees they were mentioned
+     */
 
 	it( 'checks if new user sees mention', function () {
 
-     	UserLoginPage.login( username,password );
-		//UserLoginPage.open();
+		UserLoginPage.login( username, password );
 		EchoPage.open();
 		EchoPage.alerts.click();
-        EchoPage.alertText.waitForVisible();
-        assert( EchoPage.alertText.isExisting() );
+		EchoPage.alertText.waitForVisible();
+		assert( EchoPage.alertText.isExisting() );
 
 	} );
 
-});
+} );
